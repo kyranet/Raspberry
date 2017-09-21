b68a60454fb5c01ce5b518532a9b9ab10aea1262
kyraNET <kyradiscord@gmail.com>
2017-09-21 10:59:58 +0200

    Let's ignore some events and put a disclaimer, ok?

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Raspberry
+# Raspberry
+
+This is a bot written in TS made with lots of rush, because it's a bot that runs in a Raspberry Pi Zero which purpose is nothing but waking up my PC. If you complain about me not using a command handler for this, please, don't. I have been busy doing a lot of things to bother making this fancy, plus this is a bot locked **exclusively to me**.
--- src/raspberry.ts
+++ src/raspberry.ts
@@ -90,7 +90,24 @@ class Raspberry extends Client {
 
 }
 
-new Raspberry().login(config.token);
+new Raspberry({
+    disabledEvents: [
+        'MESSAGE_REACTION_ADD',
+        'MESSAGE_REACTION_REMOVE',
+        'MESSAGE_REACTION_REMOVE_ALL',
+        'USER_UPDATE',
+        'USER_NOTE_UPDATE',
+        'USER_SETTINGS_UPDATE',
+        'PRESENCE_UPDATE',
+        'VOICE_STATE_UPDATE',
+        'TYPING_START',
+        'RELATIONSHIP_ADD',
+        'RELATIONSHIP_REMOVE',
+        'GUILD_BAN_ADD',
+        'GUILD_BAN_REMOVE'
+    ],
+    messageCacheMaxSize: 10
+}).login(config.token);
 
 type RaspberryConfig = {
     token: string;
